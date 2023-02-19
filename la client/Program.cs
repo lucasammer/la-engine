47bcae7ce1ed71a2c47d2a3a86a055c802c8d1e9
LucasAmmer <lucas@ammer.nl>
2023-02-19 14:34:25 +0100

    added Main

--- la client/Program.cs
+++ la client/Program.cs
@@ -1,2 +1,16 @@
 ﻿using la_engine;
+using static System.Net.Mime.MediaTypeNames;
 
+namespace la_engine
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ChessBoard board = new ChessBoard();
+        }
+    }
+}
